cbc2f8ad667d13fe29101e0e7617adfac20240ba
MoniraMitu <mhaque44249@gmail.com>
2023-06-07 06:04:37 -0700

    update added

---
+++ app/src/main/java/com/example/theaddresspms/EditProperty.java
@@ -0,0 +1,14 @@
+package com.example.theaddresspms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProperty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_property);
+    }
+}
